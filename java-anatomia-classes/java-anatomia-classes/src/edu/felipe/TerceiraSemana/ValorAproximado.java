7b2269bd7416697ee300bfe18e06ff3edc85bab6
Felipe Elvas Barbosa <felipe.100062@aluno.faculdadedescomplica.com.br>
2023-11-22 10:07:21 -0300

    nova tarefa

---
+++ java-anatomia-classes/java-anatomia-classes/src/edu/felipe/TerceiraSemana/ValorAproximado.java
@@ -0,0 +1,19 @@
+package edu.felipe.TerceiraSemana;
+
+public class ValorAproximado {
+	public static void main(String[] args) {
+ 
+		int x = 10;
+		int y = 6; int z = 3;
+		double a = 3; double b = 1.0;
+		double c = (double)x / y;
+		int h = (int)c;
+		
+		System.out.println("x = " + h);
+		System.out.println("y = " + y);
+		System.out.println("z = " + z);
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+}}
